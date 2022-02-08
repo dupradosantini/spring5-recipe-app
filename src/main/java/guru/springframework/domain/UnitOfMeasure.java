1738d9c3cf82895427d96b66e4fb5945cfcedabf
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-02-08 11:17:23 -0300

    Defining the Ingredient - UnitOfMeasure unidirectional OneToOne relationship, no cascades.

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,27 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String unit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
