770f10743588a088e77d01b4abb3d5bea1baa27a
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-02-08 10:55:48 -0300

    One To Many Relationship.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -23,6 +24,9 @@ public class Recipe {
 
     @OneToOne(cascade= CascadeType.ALL)// Defining the relationship type and cascade method
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     //Getters and Setters
 
