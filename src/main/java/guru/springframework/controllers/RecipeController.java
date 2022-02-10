71748f6fe2c103ce11fa3d8253dff63fb97474fb
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-02-10 12:51:44 -0300

    Integrating POST method to create a new Recipe from the Web page

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -18,5 +18,20 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new") //Method to render the view
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform"; //returning the simple string of the view name
+    }
+    //@RequestMapping(name="recipe", method = RequestMethod.POST) //Tried and tested way to do this
+    //New Way
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){ //Binding
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        //This return will redirect to the recipe saved.
+        return"redirect:/recipe/show/" + savedCommand.getId();
     }
 }
