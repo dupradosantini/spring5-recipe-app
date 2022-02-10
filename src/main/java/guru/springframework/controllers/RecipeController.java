4d92bd04d54fda775f726de7495808676a4ec649
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-02-10 15:05:06 -0300

    Adding the Delete functionality, to delete existing recipes

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,10 +2,13 @@ package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
+@Slf4j
 @Controller
 public class RecipeController {
     private final RecipeService recipeService;
@@ -13,13 +16,13 @@ public class RecipeController {
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
-
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
-
+    @GetMapping
     @RequestMapping("recipe/new") //Method to render the view
     public String newRecipe(Model model){
         model.addAttribute("recipe",new RecipeCommand());
@@ -34,9 +37,18 @@ public class RecipeController {
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeform";
+    }
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+
+        log.debug("Deleting id: "+id);
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 }
--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -73,4 +73,16 @@ public class RecipeServiceImplTest {
         verify(recipeRepository,never()).findById(anyLong());
     }
 
+    @Test
+    public  void testDeleteById() /* throws Exception */{
+        //given
+        Long idToDelete = Long.valueOf(2L);
+        //when
+        recipeService.deleteById(idToDelete);
+
+        //no when, since this method has void return type
+        //then
+        verify(recipeRepository,times(1)).deleteById(anyLong());
+    }
+
 }
